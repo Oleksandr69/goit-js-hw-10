b97e17b3cca0daf190cf4c5cb0aa7b47387ca4ba
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-02-10 21:25:22 +0200

    snackbar done my be

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,31 +2,62 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import pathSuccessIcon from '../img/ok.svg';
-import pathErrorIcon from '../img/error.svg';
+import pathErrorIcon from '../img/octagon.svg';
 let status = true;
+let backgroundSnack = '';
+let titleSnack = '';
+let messageSnack = '';
+
 const iconUrl = status ? pathSuccessIcon : pathErrorIcon;
 
 const formSnackbar = document.querySelector('.form');
 
 formSnackbar.addEventListener('submit', evt => {
   evt.preventDefault();
-  const dataSnackbar = new FormData(formSnackbar);
+  let dataSnackbar = new FormData(formSnackbar);
+  let statusSnackbar = [];
   for (const event of dataSnackbar) {
-    console.log(event);
+    statusSnackbar.push(event);
   }
+
+  const alertPromis = createPromise(statusSnackbar[1][1], statusSnackbar[0][1]);
+  alertPromis.then(data => {
+    if (data === 'fulfilled') {
+      status = true;
+      titleSnack = 'OK';
+      messageSnack = `Fulfilled promise in ${statusSnackbar[0][1]}ms`;
+      backgroundSnack = ' #59a10d';
+    } else if (data === 'rejected') {
+      status = false;
+      titleSnack = 'Error';
+      messageSnack = `Rejected promise in ${statusSnackbar[0][1]}ms`;
+      backgroundSnack = ' #ef4040';
+    }
+    messageAllert(status, titleSnack, messageSnack, backgroundSnack);
+  });
 });
 
-function messageAllert() {
+function createPromise(value, delay) {
+  const promis = new Promise(resolve => {
+    setTimeout(() => {
+      resolve(value);
+    }, delay);
+  });
+  return promis;
+}
+
+function messageAllert(status, titleSnack, messageSnack, backgroundSnack) {
+  const iconUrl = status ? pathSuccessIcon : pathErrorIcon;
   iziToast.show({
     position: 'topRight',
-    title: 'Error',
+    title: titleSnack,
     titleColor: 'white',
-    message: 'Please choose a date in the future',
+    message: messageSnack,
     messageSize: '16px',
     messageLineHeight: '24px',
     messageColor: 'white',
-    iconUrl: pathErrorIcon,
-    backgroundColor: '#EF4040',
+    iconUrl: iconUrl,
+    backgroundColor: backgroundSnack,
     theme: 'dark',
   });
 }
