4ba138c2cd483ba91e31f86afeb0b4b535e2b9af
Oleksandr69 <shevchol69@gmail.com>
2025-02-10 11:01:03 +0000

    Deploying to gh-pages from @ Oleksandr69/goit-js-hw-10@a85b8263fddd590c73b6bfb9034edf90b04a4eb4 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import"./assets/vendor-A92OCY9B.js";const o=document.querySelector(".form");o.addEventListener("submit",t=>{t.preventDefault();const e=new FormData(o);for(const n of e)console.log(n)});
+//# sourceMappingURL=2-snackbar.js.map
