30f740fa1fb58b403008c368128f899fd7bc362a
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-02-12 21:47:20 +0200

    error correction 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,16 +3,12 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 import pathSuccessIcon from '../img/ok.svg';
 import pathErrorIcon from '../img/octagon.svg';
-let status = true;
-let backgroundSnack = '';
-let titleSnack = '';
-let messageSnack = '';
-
-const iconUrl = status ? pathSuccessIcon : pathErrorIcon;
 
 const formSnackbar = document.querySelector('.form');
 
-formSnackbar.addEventListener('submit', evt => {
+formSnackbar.addEventListener('submit', snackbarStart);
+
+function snackbarStart(evt) {
   evt.preventDefault();
   let dataSnackbar = new FormData(formSnackbar);
   let statusSnackbar = [];
@@ -20,38 +16,43 @@ formSnackbar.addEventListener('submit', evt => {
     statusSnackbar.push(event);
   }
 
-  const valueSnack = statusSnackbar[1][1];
-  const delaySnack = +statusSnackbar[0][1];
+  const snackbarObj = {
+    value: statusSnackbar[1][1],
+    delay: +statusSnackbar[0][1],
+    status: true,
+  };
+  // const valueSnack = statusSnackbar[1][1];
+  // const delaySnack = +statusSnackbar[0][1];
 
-  const alertPromis = createPromise(valueSnack, delaySnack);
+  const alertPromis = createPromise(snackbarObj);
   alertPromis
-    .then((status, delaySnack) => messageAllert(status, delaySnack))
-    .catch((status, delaySnack) => messageAllert(status, delaySnack));
+    .then(snackbarObj => messageAllert(snackbarObj))
+    .catch(snackbarObj => messageAllert(snackbarObj));
   formSnackbar.reset();
-});
+}
 
-function createPromise(value, delay) {
+function createPromise(objectSnack) {
   const promis = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (value === 'fulfilled') {
-        status = true;
-        resolve(status, delay);
-      } else if (value === 'rejected') {
-        status = false;
-        reject(status, delay);
+      if (objectSnack.value === 'fulfilled') {
+        objectSnack.status = true;
+        resolve(objectSnack);
+      } else if (objectSnack.value === 'rejected') {
+        objectSnack.status = false;
+        reject(objectSnack);
       }
-    }, delay);
+    }, objectSnack.delay);
   });
   return promis;
 }
 
-function messageAllert(status, delay) {
-  const iconUrl = status ? pathSuccessIcon : pathErrorIcon;
-  const titleSnack = status ? 'OK' : 'Error';
-  const messageSnack = status
-    ? `Fulfilled promise in ${delay}ms`
-    : `Rejected promise in ${delay}ms`;
-  const backgroundSnack = status ? ' #59a10d' : ' #ef4040';
+function messageAllert(objectSnack) {
+  const iconUrl = objectSnack.status ? pathSuccessIcon : pathErrorIcon;
+  const titleSnack = objectSnack.status ? 'OK' : 'Error';
+  const messageSnack = objectSnack.status
+    ? `Fulfilled promise in ${objectSnack.delay}ms`
+    : `Rejected promise in ${objectSnack.delay}ms`;
+  const backgroundSnack = objectSnack.status ? ' #59a10d' : ' #ef4040';
   iziToast.show({
     position: 'topRight',
     title: titleSnack,
